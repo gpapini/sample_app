bd06d80eb715663f271d20a643002e67ca6e06f2
George Papini <gpapini@optonine.net>
2010-09-15 21:25:57 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+gem 'mysql'
+
+group :development do
+  gem 'rspec-rails', '2.0.0.beta.18'
+end
+
+group :test do
+  gem 'rspec', '2.0.0.beta.18'
+  gem 'spork', '0.8.4'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd8fa3b80e04461769c76e63a3b0f588626766027275a284e8f1eb87c0fb7c1880ef28dfb3ddf1f4e235e88a59554b73e6a361fe7592772f7a8d888a02b5f6ac5'
